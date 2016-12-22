bf29c7f71d1306801c14f1a50d477514b68feed0
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-12-23 00:50:57 +0200

    Fix after_success script

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -5,7 +5,7 @@ PREFIX=$(readlink -enq "$(dirname $0)/../")
 rm -rf   "$PREFIX/.gcov"
 mkdir -p "$PREFIX/.gcov"
 
-(cd test && gcov -ablpr -s "$PREFIX" -o test/.build/ test/*.cpp;  mv *.gcov "$PREFIX/.gcov/")
+(cd test && gcov -ablpr -s "$PREFIX" -o .build/ *.cpp;   mv *.gcov "$PREFIX/.gcov/")
 gcov -ablpr -s "$PREFIX" -o shared/zend/   zend/*.cpp;   mv *.gcov "$PREFIX/.gcov/"
 gcov -ablpr -s "$PREFIX" -o shared/common/ common/*.cpp; mv *.gcov "$PREFIX/.gcov/"
 
