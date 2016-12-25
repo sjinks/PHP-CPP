6d0e8d93a5e05a57b429a1c535c56a05a195bcdb
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-12-25 02:19:43 +0200

    FunctionsWithParameters

--- test/test0001.cpp
+++ test/test0001.cpp
@@ -22,9 +22,63 @@ static void test1_my_function_void()
     Php::out << "In test1_my_function_void()" << std::endl;
 }
 
+void test1_my_with_undefined_parameters_function(Php::Parameters &params)
+{
+    for (unsigned int i=0; i<params.size(); ++i) {
+        Php::out << "Parameter " << i << ": " << params[i] << std::endl;
+    }
+}
+
+Php::Value test1_my_with_defined_parameters_function(Php::Parameters &params)
+{
+    for (unsigned int i=0; i<params.size(); ++i) {
+        Php::out << "Parameter " << i << ": " << params[i] << std::endl;
+    }
+
+    return params[0] + params[1];
+}
+
+void test1_my_with_defined_parameters_reference_function(Php::Parameters &params)
+{
+    params[0] = "I changed!";
+}
+
+void test1_my_with_defined_array_parameters_function(Php::Parameters &params)
+{
+    for (int i=0; i<params[0].size(); ++i) {
+        Php::out << "The array: " << params[0][i] << std::endl;
+    }
+}
+
+void test1_my_with_defined_object_parameters_function(Php::Parameters &params)
+{
+    for (unsigned int i=0; i<params.size(); ++i) {
+        Php::out << params[i] << std::endl;
+    }
+}
+
+
 void init_Test0001(Php::Extension& e)
 {
     e.add<my_add>("my_add");
     e.add<test1_my_no_parameters_function>("test1_my_no_parameters_function");
     e.add<test1_my_function_void>("test1_my_function_void");
+
+    e.add<test1_my_with_undefined_parameters_function>("test1_my_with_undefined_parameters_function");
+    e.add<test1_my_with_defined_parameters_function>("test1_my_with_defined_parameters_function", {
+        Php::ByVal("x", Php::Type::Numeric),
+        Php::ByVal("y", Php::Type::Numeric)
+    });
+
+    e.add<test1_my_with_defined_parameters_reference_function>("test1_my_with_defined_parameters_reference_function", {
+        Php::ByRef("string", Php::Type::String)
+    });
+
+    e.add<test1_my_with_defined_array_parameters_function>("test1_my_with_defined_array_parameters_function", {
+        Php::ByVal("array", Php::Type::Array)
+    });
+
+    e.add<test1_my_with_defined_object_parameters_function>("test1_my_with_defined_object_parameters_function", {
+        Php::ByVal("myClassObjVar", "MyPhpClass")
+    });
 }
--- test/tests/0001.phpt
+++ test/tests/0001.phpt
@@ -7,8 +7,57 @@ Call extension functions from PHP
 echo my_add(1, 2, 3, 4, 5), PHP_EOL;
 echo test1_my_no_parameters_function(), PHP_EOL;
 test1_my_function_void();
+
+class MyPhpClass 
+{
+    public $aMemberVar = "aMemberVar";
+
+    public function __toString()
+    {
+        return "myPhpClass.__toString()";
+    }
+
+    public function getMemberVar()
+    {
+        return $this->aMemberVar;
+    }
+}
+
+echo PHP_EOL;
+echo test1_my_with_undefined_parameters_function('1st', '2nd', '3rd', '4th'), PHP_EOL, PHP_EOL;
+echo test1_my_with_defined_parameters_function(21, 42), PHP_EOL, PHP_EOL;
+$referenceVar = "I am unchanged.";
+echo "The value of referenceVar: ", $referenceVar, PHP_EOL;
+echo test1_my_with_defined_parameters_reference_function($referenceVar), PHP_EOL;
+echo "New value of referenceVar: ", $referenceVar, PHP_EOL, PHP_EOL;
+$myArray = array("a", "b", "c", "d");
+echo test1_my_with_defined_array_parameters_function($myArray), PHP_EOL, PHP_EOL;
+$myPhpClass = new MyPhpClass;
+echo test1_my_with_defined_object_parameters_function($myPhpClass);
 ?>
 --EXPECT--
 15
 42
 In test1_my_function_void()
+
+Parameter 0: 1st
+Parameter 1: 2nd
+Parameter 2: 3rd
+Parameter 3: 4th
+
+
+Parameter 0: 21
+Parameter 1: 42
+63
+
+The value of referenceVar: I am unchanged.
+
+New value of referenceVar: I changed!
+
+The array: a
+The array: b
+The array: c
+The array: d
+
+
+myPhpClass.__toString()
